adb21e6fe4418b0c8e20d2696fc065d3feb1b55d
Cedric <cedric.todaro@gmail.com>
2025-11-02 18:53:04 +0100

    dim. 02 nov. 2025 18:52:48 CET

--- .dev/st/config.h
+++ .dev/st/config.h
@@ -94,7 +94,7 @@ char* termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
-float alpha = 1;
+float alpha = 0.85;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char* colorname[] = {
@@ -177,8 +177,10 @@ static uint forcemousemod = ShiftMask;
  */
 static MouseShortcut mshortcuts[] = {
     /* mask                 button   function        argument       release */
-    { XK_ANY_MOD, Button2, selpaste, { .i = 0 }, 1 },  { ShiftMask, Button4, ttysend, { .s = "\033[5;2~" } },
-    { XK_ANY_MOD, Button4, ttysend, { .s = "\031" } }, { ShiftMask, Button5, ttysend, { .s = "\033[6;2~" } },
+    { XK_ANY_MOD, Button2, selpaste, { .i = 0 }, 1 },
+    { ShiftMask, Button4, ttysend, { .s = "\033[5;2~" } },
+    { XK_ANY_MOD, Button4, ttysend, { .s = "\031" } },
+    { ShiftMask, Button5, ttysend, { .s = "\033[6;2~" } },
     { XK_ANY_MOD, Button5, ttysend, { .s = "\005" } },
 };
 
@@ -188,13 +190,20 @@ static MouseShortcut mshortcuts[] = {
 
 static Shortcut shortcuts[] = {
     /* mask                 keysym          function        argument */
-    { XK_ANY_MOD, XK_Break, sendbreak, { .i = 0 } },   { ControlMask, XK_Print, toggleprinter, { .i = 0 } },
-    { ShiftMask, XK_Print, printscreen, { .i = 0 } },  { XK_ANY_MOD, XK_Print, printsel, { .i = 0 } },
-    { TERMMOD, XK_Prior, zoom, { .f = +1 } },          { TERMMOD, XK_Next, zoom, { .f = -1 } },
-    { TERMMOD, XK_Home, zoomreset, { .f = 0 } },       { TERMMOD, XK_C, clipcopy, { .i = 0 } },
-    { TERMMOD, XK_V, clippaste, { .i = 0 } },          { TERMMOD, XK_Y, selpaste, { .i = 0 } },
-    { ShiftMask, XK_Insert, selpaste, { .i = 0 } },    { TERMMOD, XK_Num_Lock, numlock, { .i = 0 } },
-    { ShiftMask, XK_Page_Up, kscrollup, { .i = -1 } }, { ShiftMask, XK_Page_Down, kscrolldown, { .i = -1 } },
+    { XK_ANY_MOD, XK_Break, sendbreak, { .i = 0 } },
+    { ControlMask, XK_Print, toggleprinter, { .i = 0 } },
+    { ShiftMask, XK_Print, printscreen, { .i = 0 } },
+    { XK_ANY_MOD, XK_Print, printsel, { .i = 0 } },
+    { TERMMOD, XK_Prior, zoom, { .f = +1 } },
+    { TERMMOD, XK_Next, zoom, { .f = -1 } },
+    { TERMMOD, XK_Home, zoomreset, { .f = 0 } },
+    { TERMMOD, XK_C, clipcopy, { .i = 0 } },
+    { TERMMOD, XK_V, clippaste, { .i = 0 } },
+    { TERMMOD, XK_Y, selpaste, { .i = 0 } },
+    { ShiftMask, XK_Insert, selpaste, { .i = 0 } },
+    { TERMMOD, XK_Num_Lock, numlock, { .i = 0 } },
+    { ShiftMask, XK_Page_Up, kscrollup, { .i = -1 } },
+    { ShiftMask, XK_Page_Down, kscrolldown, { .i = -1 } },
 };
 
 /*
