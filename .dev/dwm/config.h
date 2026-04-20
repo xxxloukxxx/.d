f15a84dd7e0fa5db64429a068d5df2a3954162e4
Cedric <cedric.todaro@gmail.com>
2026-04-20 23:28:03 +0200

    Fix

--- .dev/dwm/config.h
+++ .dev/dwm/config.h
@@ -10,8 +10,8 @@ static const unsigned int systraypinning = 1; /* 0: sloppy systray follows selec
 static const unsigned int systrayonleft  = 0; /* 0: systray in the right corner, >0: systray on left of status text */
 static const unsigned int systrayspacing = 2; /* systray spacing */
 static const int systraypinningfailfirst = 1; /* 1: if pinning fails, display systray on the first monitor, */
-/*                                                  False: display systray on the last monitor*/
-static const int showsystray  = 1; /* 0 means no systray */
+                                              /* False: display systray on the last monitor*/
+static const int showsystray  = 1;            /* 0 means no systray */
 static const int focusonwheel = 0;
 static const char* fonts[]    = { "agave:size=12" };
 static const char dmenufont[] = "agave:size=12";
@@ -38,7 +38,8 @@ static const Rule rules[] = {
      *  WM_NAME(STRING) = title
      */
     /* class      instance    title       tags mask     isfloating   monitor */
-    { "vesktop", NULL, NULL, 1 << 4, 0, 0 },
+    // { "vesktop", NULL, NULL, 1 << 4, 0, 0 },
+    0
 };
 
 /* layout(s) */
