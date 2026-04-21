547b91e07207b19ba1552b6066427610515c8a53
Cedric <cedric.todaro@gmail.com>
2026-04-21 18:42:19 +0200

    hack 14

--- .dev/st/config.h
+++ .dev/st/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char* font   = "hack:size=16";
+static char* font   = "hack:size=14";
 static int borderpx = 0;
 
 /*
