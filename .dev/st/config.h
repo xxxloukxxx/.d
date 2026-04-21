21521ee3c8b1ca9faa07d80041923fc6be187bd9
Cedric <cedric.todaro@gmail.com>
2026-04-21 18:45:58 +0200

    agave 14

--- .dev/st/config.h
+++ .dev/st/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char* font   = "hack:size=14";
+static char* font   = "agave:size=14";
 static int borderpx = 0;
 
 /*
